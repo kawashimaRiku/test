4b7ad5860ffda95f8618c71b7afec1ecb85e4e3a
riku231027 <Riku20231027@gmail.com>
2026-06-11 18:19:37 +0900

    app.js

--- myapp/app.js
+++ myapp/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var helloRouter = require('./routes/hello');
+var catRouter = require('./routes/cat');
+var qrRouter = require('./routes/qr');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hello', helloRouter);
+app.use('/cat', catRouter);
+app.use('/qr', qrRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
